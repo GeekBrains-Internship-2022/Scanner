dc61cb3713ea6b378c660946317a5360107b63c9
Alex <kpuk77@bk.ru>
2022-01-29 03:17:06 +0800

    Начало Observer Service

---
+++ UI/Scanner/Service/ObserverService.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+namespace Scanner.Service
+{
+    public class ObserverService
+    {
+        private readonly string _Path;
+
+        public ObserverService(string path) => _Path = path;
+
+        void Start()
+        {
+            using var watcher = new FileSystemWatcher(_Path)
+            {
+                NotifyFilter = NotifyFilters.LastWrite,
+                Filter = "*.pdf",
+                IncludeSubdirectories = true,
+                EnableRaisingEvents = true,
+            };
+
+            watcher.Created += OnCreated;
+            watcher.Error += OnError;
+        }
+
+        private void OnError(object sender, ErrorEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
