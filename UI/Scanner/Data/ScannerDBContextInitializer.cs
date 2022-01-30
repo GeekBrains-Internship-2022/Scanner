ad0720c36180dfbbeafc73fcc68ce981e3d23dd7
Дима Ищенко <yanusodron@ya.ru>
2022-01-30 16:59:26 +0500

    Добавил класс ScannerDBContextInitializer для миграций

---
+++ UI/Scanner/Data/ScannerDBContextInitializer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.IO;
+
+namespace Scanner.Data
+{
+    class ScannerDBContextInitializer : IDesignTimeDbContextFactory<ScannerDB>
+    {
+        public ScannerDB CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ScannerDB>();
+            optionsBuilder.UseSqlite($"Filename=.\\ScannerDB.db");
+            return new ScannerDB(optionsBuilder.Options);
+        }
+    }
+}
