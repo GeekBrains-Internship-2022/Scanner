65451616586806c8a1a0dedb9c6f822f4033584a
Alex <kpuk77@bk.ru>
2022-02-01 20:29:58 +0800

    Start rabbit refactoring..

---
+++ UI/Scanner/Service/RabbitMQ/RabbitMQConnection.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Extensions.Logging;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+using Scanner.interfaces;
+
+using System;
+using System.IO;
+
+namespace Scanner.Service
+{
+    public class RabbitMQConnection : IRabbitMQPersistentConnection
+    {
+        private readonly IConnectionFactory _ConnectionFactory;
+        private readonly ILogger<RabbitMQConnection> _Logger;
+        private IConnection _Connection;
+        private bool _Disposed;
+
+        private readonly object _Lock = new();
+
+        public RabbitMQConnection(IConnectionFactory connectionFactory, ILogger<RabbitMQConnection> logger)
+        {
+            _ConnectionFactory = connectionFactory;
+            _Logger = logger;
+        }
+
+        public bool IsConnected => _Connection is not null && _Connection.IsOpen && !_Disposed;
+
+        public IModel CreateModel()
+        {
+            if (!IsConnected)
+            {
+                var e = "No RabbitMQ connections are available to perform this action";
+                _Logger.LogError(e);
+                throw new InvalidOperationException(e);
+            }
+
+            return _Connection.CreateModel();
+        }
+
+        public void Dispose()
+        {
+            if(_Disposed) return;
+            _Disposed = true;
+            try
+            {
+                _Connection.Dispose();
+            }
+            catch (IOException e)
+            {
+                _Logger.LogError(e.Message);
+            }
+        }
+
+        public bool TryConnect()
+        {
+            _Logger.LogInformation("RabbitMQ Client is trying to connect");
+
+            lock (_Lock)
+            {
+                _Connection = _ConnectionFactory.CreateConnection();
+            }
+
+            if (IsConnected)
+            {
+                _Connection.ConnectionShutdown += OnConnectionShutdown;
+                _Connection.CallbackException += OnCallbackException;
+                _Connection.ConnectionBlocked += OnConnectionBlocked;
+            }
+
+            return true;
+        }
+
+        private void OnConnectionBlocked(object? sender, ConnectionBlockedEventArgs e)
+        {
+            if (_Disposed) return;
+
+            _Logger.LogWarning("A RabbitMQ connection is shutdown. Trying to re-connect...");
+
+            TryConnect();
+        }
+
+        private void OnCallbackException(object? sender, CallbackExceptionEventArgs e)
+        {
+            if (_Disposed) return;
+            
+            _Logger.LogWarning("A RabbitMQ connection throw exception. Trying to re-connect...");
+
+            TryConnect();
+        }
+
+        private void OnConnectionShutdown(object? sender, ShutdownEventArgs e)
+        {
+            if (_Disposed) return;
+
+            _Logger.LogWarning("A RabbitMQ connection is on shutdown. Trying to re-connect...");
+
+            TryConnect();
+        }
+    }
+}
---
+++ UI/Scanner/interfaces/IRabbitMQPersistentConnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace Scanner.interfaces
+{
+    public interface IRabbitMQPersistentConnection : IDisposable
+    {
+        bool IsConnected { get; }
+        bool TryConnect();
+        IModel CreateModel();
+    }
+}
