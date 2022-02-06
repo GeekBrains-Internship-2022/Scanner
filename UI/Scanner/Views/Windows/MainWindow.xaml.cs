91bc3843080827d6dc39265273d42c680766cc0b
Sergey Kudryashov <serjikf@yandex.ru>
2022-02-06 19:58:25 +0300

    Тестовое отоброжение страницы

--- UI/Scanner/Views/Windows/MainWindow.xaml.cs
+++ UI/Scanner/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Cube.Pdf.Pdfium;
+
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 using Scanner.interfaces;
@@ -7,10 +9,14 @@ using Scanner.Models;
 using Scanner.Service;
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace Scanner.Views.Windows
 {
@@ -23,14 +29,14 @@ namespace Scanner.Views.Windows
         {
             InitializeComponent();
 
-            MethodForTest();
+            PdfViewTest(imgDocument);
         }
 
         #region For test without ui
 
         private static readonly IConfiguration __Configuration = App.Services.GetRequiredService<IConfiguration>();
 
-        private static void MethodForTest()
+        private static void MethodForTest(System.Windows.Controls.Image image)
         {
             //ObserverTest();
             //FileServiceTest();
@@ -78,6 +84,24 @@ namespace Scanner.Views.Windows
             fileService.Move(filePath, fileName: fileData.Guid.ToString());
         }
 
+        private static void PdfViewTest(System.Windows.Controls.Image image)
+        {
+            string tempfilePath = "..\\..\\..\\Образец.pdf";            
+            System.Drawing.Image imgPage;
+            DocumentReader documentReader;
+
+            using (DocumentRenderer documentRenderer = new DocumentRenderer(tempfilePath))
+            {
+                documentReader = new DocumentReader(tempfilePath);
+                var pages = documentReader.Pages.ToArray();
+                imgPage = documentRenderer.Render(pages[0], pages[0].Size);
+
+                Bitmap bitmap = new Bitmap(imgPage);
+                BitmapSource bmpResource = getBitMapSourceFromBitmap(bitmap);
+                image.Source = bmpResource;
+            }
+        }
+
         #endregion
 
         private void mniSettings_Click(object sender, RoutedEventArgs e)
@@ -90,5 +114,41 @@ namespace Scanner.Views.Windows
         {
             System.Environment.Exit(0);
         }
+
+        [DllImport("gdi32")]
+        static extern int DeleteObject(IntPtr o);
+        /// <summary>
+        /// Bitmap->BitmapSource
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <returns></returns>
+        public static BitmapSource getBitMapSourceFromBitmap(Bitmap bitmap)
+        {
+            IntPtr intPtrl = bitmap.GetHbitmap();
+            BitmapSource bitmapSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(intPtrl,
+                IntPtr.Zero,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+            DeleteObject(intPtrl);
+            return bitmapSource;
+        }
+
+        public byte[] ConvertBitMapToByteArray(Bitmap bitmap)
+        {
+            byte[] result = null;
+
+            if (bitmap != null)
+            {
+                ImageConverter converter = new ImageConverter();
+                result = (byte[])converter.ConvertTo(bitmap, typeof(byte[]));
+
+                //MemoryStream stream = new MemoryStream();
+                //bitmap.Save(stream, bitmap.RawFormat);
+                //result = stream.ToArray();
+            }
+
+            return result;
+        }
+
     }
 }
