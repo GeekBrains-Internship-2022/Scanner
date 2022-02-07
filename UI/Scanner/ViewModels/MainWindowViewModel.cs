697a816ee208bb48b72c51d4d36644c3e2fb7d1d
Дима Ищенко <yanusodron@ya.ru>
2022-02-07 21:43:47 +0500

    Добавил модель отображения главного окна

---
+++ UI/Scanner/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Scanner.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scanner.ViewModels
+{
+    class MainWindowViewModel: ViewModel
+    {
+    }
+}
