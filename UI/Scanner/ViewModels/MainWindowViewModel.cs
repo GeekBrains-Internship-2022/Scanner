877434846f5b2cbd2128fbbe23721c121889f3ea
Sergey Kudryashov <serjikf@yandex.ru>
2022-02-08 22:59:28 +0300

    nothing significant

--- UI/Scanner/ViewModels/MainWindowViewModel.cs
+++ UI/Scanner/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Scanner.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +12,13 @@ namespace Scanner.ViewModels
     {
         private string _title = "Сканировщик";
 
+        public IList<string> ScaningDocuments { get; } = new ObservableCollection<string>(); //Список отсканированных файлов
+
         public string Title { get => _title; set => Set(ref _title, value); }
 
         public MainWindowViewModel()
         {
-
+            ScaningDocuments.Add("паспорт.pdf");
         }
     }
 }
