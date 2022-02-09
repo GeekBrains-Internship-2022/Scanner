e8125a54c2798069bbaa0216579ea49bedd36d72
Kusn <serjikf@yandex.ru>
2022-02-09 10:52:23 +0300

    Добавлены классы отсканированного документа и фильтра во вьюмодель

--- UI/Scanner/ViewModels/MainWindowViewModel.cs
+++ UI/Scanner/ViewModels/MainWindowViewModel.cs
@@ -12,13 +12,40 @@ namespace Scanner.ViewModels
     {
         private string _title = "Сканировщик";
 
-        public IList<string> ScaningDocuments { get; } = new ObservableCollection<string>(); //Список отсканированных файлов
+        public IList<ScanDocument> ScaningDocuments { get; } = new ObservableCollection<ScanDocument>(); //Список отсканированных файлов
+        public IList<DocumetnFilter> DocumetnFilters { get; } = new ObservableCollection<DocumetnFilter>(); //Список фильтров
 
         public string Title { get => _title; set => Set(ref _title, value); }
 
         public MainWindowViewModel()
         {
-            ScaningDocuments.Add("паспорт.pdf");
+            ScaningDocuments.Add(new ScanDocument { Date = DateTime.Today, FilePath = "", Name = "1", Type = "Паспорт" });
+            ScaningDocuments.Add(new ScanDocument { Date = DateTime.Today, FilePath = "", Name = "2", Type = "Диплом" });
+
+            DocumetnFilters.Add(new DocumetnFilter { FilterName = "По имени" });
+            DocumetnFilters.Add(new DocumetnFilter { FilterName = "По типу" });
+            DocumetnFilters.Add(new DocumetnFilter { FilterName = "По времени" });
+        }
+
+        private void DeleteScanDocument(ScanDocument document)
+        {
+            ScaningDocuments.Remove(document);
+        }
+
+        public class ScanDocument
+        {
+            public string FilePath { get; set; }
+
+            public DateTime Date { get; set; }
+
+            public string Type { get; set; }
+
+            public string Name { get; set; }
+        }
+
+        public class DocumetnFilter
+        {
+            public string FilterName { get; set; }
         }
     }
 }
