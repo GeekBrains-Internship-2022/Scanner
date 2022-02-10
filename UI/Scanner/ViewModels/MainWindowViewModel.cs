24e579833ebbd8beff29be65a1f4b6651f05639e
Kusn <serjikf@yandex.ru>
2022-02-10 13:46:27 +0300

    Получение списка осканированных файлов

--- UI/Scanner/ViewModels/MainWindowViewModel.cs
+++ UI/Scanner/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Scanner.ViewModels.Base;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Scanner.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -10,6 +12,9 @@ namespace Scanner.ViewModels
 {
     class MainWindowViewModel: ViewModel
     {
+        private static readonly IConfiguration __Configuration = App.Services.GetRequiredService<IConfiguration>();
+        private List<string> _types = new List<string>();
+        //private List<string[]> _filesList = new List<string[]>();
         private string _title = "Сканировщик";
 
         public IList<ScanDocument> ScaningDocuments { get; } = new ObservableCollection<ScanDocument>(); //Список отсканированных файлов
@@ -20,8 +25,7 @@ namespace Scanner.ViewModels
 
         public MainWindowViewModel()
         {
-            ScaningDocuments.Add(new ScanDocument { Date = DateTime.Today, FilePath = "", Name = "1", Type = "Паспорт" });
-            ScaningDocuments.Add(new ScanDocument { Date = DateTime.Today, FilePath = "", Name = "2", Type = "Диплом" });
+            ScaningDocuments = GetScanDocuments();
 
             DocumetnFilters.Add(new DocumetnFilter { FilterName = "По имени" });
             DocumetnFilters.Add(new DocumetnFilter { FilterName = "По типу" });
@@ -33,12 +37,67 @@ namespace Scanner.ViewModels
             ScaningDocuments.Remove(document);
         }
 
+        private ObservableCollection<ScanDocument> GetScanDocuments()
+        {
+            var path = __Configuration["ObserverPath"];
+
+            ObservableCollection<ScanDocument> documents = new ObservableCollection<ScanDocument>();
+
+            string[] subDirs = null;            
+
+            try
+            {
+                subDirs = System.IO.Directory.GetDirectories(path);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (System.IO.DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            foreach (var dir in subDirs)
+            {
+                List<string[]> _filesList = new List<string[]>();
+                try
+                {
+                    _filesList.Add(System.IO.Directory.GetFiles(dir));
+                    var s = dir.Substring((path + "\\").Length);    //получаем тип из названия папки
+                    _types.Add(s);
+
+                    foreach (var files in _filesList.ToArray())
+                    {
+                        foreach (var file in files)
+                        {
+                            System.IO.FileInfo fileInfo = new System.IO.FileInfo(file);
+                            documents.Add(new ScanDocument { Date = fileInfo.CreationTime.ToShortTimeString(), FilePath = file, Name = fileInfo.Name, Type = s });
+                        }
+                    }
+                }
+
+                catch (UnauthorizedAccessException e)
+                {
+
+                    Console.WriteLine(e.Message);
+                }
+
+                catch (System.IO.DirectoryNotFoundException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }            
+
+            return documents;
+        }
+
         //Класс отсканированного документа
         public class ScanDocument
         {
             public string FilePath { get; set; }
 
-            public DateTime Date { get; set; }
+            public string Date { get; set; }
 
             public string Type { get; set; }
 
