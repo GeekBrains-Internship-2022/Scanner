b0f537a4ada0505ca75e2fdf2ac4694819643a49
Alex <kpuk77@bk.ru>
2022-02-11 15:20:23 +0800

    Добавлена lambdaCommand

---
+++ UI/Scanner/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Scanner.Infrastructure.Commands.Base;
+
+namespace Scanner.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _Execute = execute;
+            _CanExecute = canExecute;
+        }
+
+        public override void Execute(object parameter)
+        {
+            if (CanExecute(parameter))
+                _Execute(parameter);
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+    }
+}
