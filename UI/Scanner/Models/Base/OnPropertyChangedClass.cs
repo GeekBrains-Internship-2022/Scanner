5a89fe8f87677548efa004c140b1deb8ffe0311a
Alex <kpuk77@bk.ru>
2022-02-11 16:47:20 +0800

    Добавлен интерфейс INotifyPropertyChanged для модели ScanDocument, для отображения изменений в UI

---
+++ UI/Scanner/Models/Base/OnPropertyChangedClass.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Scanner.Models.Base
+{
+    public class OnPropertyChangedClass : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string PropertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+
+        protected bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
--- UI/Scanner/Models/ScanDocument.cs
+++ UI/Scanner/Models/ScanDocument.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 
+using Scanner.Models.Base;
+
 namespace Scanner.Models
 {
-    public class ScanDocument
+    public class ScanDocument : OnPropertyChangedClass
     {
-        public string Name { get; set; }
-        public string Path { get; set; }
-        public string Type { get; set; }
-        public DateTime Date { get; set; }
+        private string _Name;
+        private string _Path;
+        private string _Type;
+        private DateTime _Date;
+
+        public string Name
+        {
+            get => _Name;
+            set => Set(ref _Name, value);
+        }
+
+        public string Path
+        {
+            get => _Path;
+            set => Set(ref _Path, value);
+        }
+
+        public string Type
+        {
+            get => _Type;
+            set => Set(ref _Type, value);
+        }
+
+        public DateTime Date
+        {
+            get => _Date;
+            set => Set(ref _Date, value);
+        }
     }
 }
