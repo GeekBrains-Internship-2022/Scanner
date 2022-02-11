3926b4f1afd8e7e4bd64a02c07543af85b92870f
Alex <kpuk77@bk.ru>
2022-02-11 15:19:55 +0800

    Добавлена модель ScanDocument

---
+++ UI/Scanner/Models/ScanDocument.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scanner.Models
+{
+    public class ScanDocument
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
