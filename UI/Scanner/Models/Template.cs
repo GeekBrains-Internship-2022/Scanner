91f3cfd4f84ff0858d1ecf44962068907a47a072
Sergey Kudryashov <serjikf@yandex.ru>
2022-02-12 22:01:45 +0300

    Модель шаблона для VM

---
+++ UI/Scanner/Models/Template.cs
@@ -0,0 +1,18 @@
+﻿using Scanner.Models.Base;
+using System.Collections.ObjectModel;
+
+namespace Scanner.Models
+{
+    internal class Template : OnPropertyChangedClass
+    {
+        private string _name;
+        private bool _required;
+        private ObservableCollection<string> _metadata;
+
+        public string Name { get => _name; set => _name = value; }
+
+        public ObservableCollection<string> Metadata { get => _metadata; set => _metadata = value; }
+
+        public bool Required { get => _required; set => _required = value; }
+    }
+}
