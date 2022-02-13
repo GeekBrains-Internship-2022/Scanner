b1cf9ad33a19fe63cf52b21251532cee7bc16cba
Sergey Kudryashov <serjikf@yandex.ru>
2022-02-13 10:17:12 +0300

    Рефакторинг

--- UI/Scanner/ViewModels/Models/ScanDocument.cs
+++ UI/Scanner/ViewModels/Models/ScanDocument.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 
 using Scanner.Models.Base;
 
---
+++ UI/Scanner/ViewModels/Models/Template.cs
@@ -0,0 +1,15 @@
+﻿using Scanner.Models.Base;
+using System.Collections.ObjectModel;
+
+namespace Scanner.Models
+{
+    internal class Template : OnPropertyChangedClass
+    {
+        private string _name;        
+        private ObservableCollection<TemplateMetadata> _metadata;
+
+        public string Name { get => _name; set => _name = value; }
+
+        public ObservableCollection<TemplateMetadata> Metadata { get => _metadata; set => _metadata = value; }        
+    }
+}
