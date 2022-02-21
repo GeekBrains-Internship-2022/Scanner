c9bebeffd64008af562b32d5b439128ca5eef56c
Sergey Kudryashov <serjikf@yandex.ru>
2022-02-21 18:04:14 +0300

    Миграция FileIsChecked

--- UI/Scanner/Data/Migrations/20220221150306_FileIsChecked.Designer.cs
+++ UI/Scanner/Data/Migrations/20220221150306_FileIsChecked.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Scanner.Data;
 
 namespace Scanner.Data.Migrations
 {
     [DbContext(typeof(ScannerDB))]
-    partial class ScannerDBModelSnapshot : ModelSnapshot
+    [Migration("20220221150306_FileIsChecked")]
+    partial class FileIsChecked
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
